61a24f38a037cb8f41e8fc7891b5706b1fa78224
Alexa Billings <alexa.billings@gmail.com>
2018-01-18 12:15:44 -0600

    map renders on single component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginForm from './User/LoginForm'
 import SignupForm from './User/SignupForm'
 import Landing from './Landing'
 import firebase from './firebase'
+import worldMap from './worldMap'
 
 //this is like main, you put your components in this
 
@@ -23,6 +24,9 @@ class App extends Component {
           exact path="/login"
           component={LoginForm} />
 
+        <Route
+          exact path="/map"
+          component={worldMap}/>
       </div>
       </Router>
     )
